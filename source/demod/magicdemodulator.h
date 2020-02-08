035518e1d1981d3d3a0e252a5406ed7a00c3ff3d
Tibor Kalman <tibor.kalman@avatao.com>
2020-02-08 16:11:01 +0100

    Add sync to MagicDemodulator

--- source/demod/magicdemodulator.h
+++ source/demod/magicdemodulator.h
@@ -17,19 +17,19 @@ public:
     void clear();
 
 private:
-    static constexpr int PACKET_SIZES_COUNT = 3;
+    static constexpr int PACKET_SIZES_COUNT = 4;
 
     long sampling_rate;
     long datarate;
     QString source_string;
 
-    int packet_lengths[3] = {260, 333, 514};
-    QVector<char> packet_buffers[3] = {QVector<char>(), QVector<char>(), QVector<char>()};
+    int packet_lengths[4] = {70, 260, 333, 514};
+    QVector<char> packet_buffers[4] = {QVector<char>(), QVector<char>(), QVector<char>()};
 
     AveragingVariables avg_vars;
     AveragingDecVariables avg_dec_vars;
     DemodulationVariables demod_vars;
-    DecisionVariables dec_vars[3];
+    DecisionVariables dec_vars[4];
 
     std::complex<float> dem_a;
     std::complex<float> dem_b;
